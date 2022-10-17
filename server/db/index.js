39513fb68000b70a430b60fbe2ee3583138ed54b
Tudor Bostan <tudor.b@goparrot.ai>
2022-10-17 17:03:15 +0300

    feat(docker-compose): add pgpol

--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,12 @@
 const { Pool } = require('pg')
 
 const pool = new Pool(
-    // {
-    // user: process.env.PGUSER,
-    // host: process.env.PGHOST,
-    // database: process.env.PGDATABASE,
-    // password: process.env.PGPASSWORD,
-    // port: process.env.PGPORT,   
-// }
+    {
+    user: 'postgres',
+    host: 'pgpool',
+    database: 'customdatabase',
+    password: 'adminpassword',
+}
 );
 
 module.exports = {
